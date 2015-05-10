29eb3dcc69f2b23c76aa1f4c3d0a684280dbfcef
Arne Bethmann <bethmann@uni-mannheim.de>
2015-05-10 22:57:42 +0200

    bug fixing and stream lining

--- bootstrap.sh
+++ bootstrap.sh
@@ -1,19 +1,19 @@
 #!/usr/bin/env bash
 
-## Copy new apt sources list to VM
+echo "Copy new apt sources list to VM ..."
 cp /vagrant/conf/sources.list /etc/apt/sources.list
 
-## install key for CRAN repository
+echo "Install key for CRAN repository ..."
 apt-key adv --keyserver keyserver.ubuntu.com --recv-keys E084DAB9
 
-## install recent basic ubuntu upgrades
+echo "Install recent Ubuntu upgrades ..."
 apt-get update
 apt-get upgrade -y
 
-## install basic GUI
+echo "Install basic GUI ..."
 apt-get install -y joe aptitude htop mc lxde virtualbox-guest-dkms virtualbox-guest-utils virtualbox-guest-x11 firefox firefox-locale-de libreoffice libreoffice-l10n-de gimp
 
-## install German language and set as default
+echo "Install German language and set as default ..."
 apt-get install -y language-pack-de language-pack-de-base language-pack-gnome-de wngerman wogerman wswiss 
 dpkg-reconfigure locales
 setxkbmap -model pc105 -layout de -variant basic
@@ -21,38 +21,40 @@ timedatectl set-timezone Europe/Berlin
 locale-gen de_DE.UTF-8
 update-locale LANG=de_DE.UTF-8
 
-## fix bug
+echo "Fix dictionaries bug ..."
 /usr/share/debconf/fix_db.pl
 
-## install R from cran repository
+echo "Install R from cran repository ..."
 apt-get install -y r-base r-base-dev libjpeg62 r-base-core r-base-html r-recommended libgomp1 r-cran-mgcv libjpeg-dev libpng12-dev libgstreamer0.10-0 libgstreamer-plugins-base0.10-0
 
-## install RStudio
-wget --quiet http://download1.rstudio.org/rstudio-0.98.1103-amd64.deb
-dpkg -i rstudio-0.98.1103-amd64.deb
+echo "Install RStudio ..."
+#wget --quiet http://download1.rstudio.org/rstudio-0.98.1103-amd64.deb
+#dpkg -i rstudio-0.98.1103-amd64.deb
 
-## install git
+echo "Install git ..."
 apt-get install -y git gitk git-gui gitg
 
-## install spyder - just in case ;)
+echo "Install spyder ..."
 apt-get install -y spyder spyder3
 
-## install LaTeX
+echo "Install LaTeX ..."
 apt-get install -y texmaker jabref texlive-base texlive-latex-base texlive-lang-german
 
-## fix lxdm shutdown bug
+echo "Fix lxdm shutdown bug ..."
 echo "session required pam_systemd.so" >> /etc/pam.d/lxdm
 
-## copy UMASDS specific config files
+echo "Copy UMASDS specific config files ..."
 cp /vagrant/conf/xorg.conf /etc/X11/xorg.conf
 cp /vagrant/conf/sds.png /home/vagrant/sds.png
 mkdir -p /home/vagrant/.config/lxterminal
 cp /vagrant/conf/lxterminal.conf /home/vagrant/.config/lxterminal/
-chown -R vagrant:vagrant /home/vagrant/.config
 cp /vagrant/conf/lxdm.conf /etc/lxdm/default.conf
 cp /vagrant/conf/firefox.js /etc/firefox
+mkdir -p /home/vagrant/.config/pcmanfm/LXDE
+cp /vagrant/conf/desktop-items-0.conf /home/vagrant/.config/pcmanfm/LXDE/desktop-items-0.conf
+chown -R vagrant:vagrant /home/vagrant/.config
 
-## set some convience links
+echo "Set some convience links ..."
 ln -sf /vagrant /home/vagrant/Dokumente
 mkdir -p /home/vagrant/Desktop
 chown -R vagrant:vagrant /home/vagrant/Desktop
@@ -70,6 +72,3 @@ cp /usr/share/applications/jabref.desktop /home/vagrant/Desktop/
 cp /usr/share/applications/libreoffice-writer.desktop /home/vagrant/Desktop/
 cp /usr/share/applications/libreoffice-calc.desktop /home/vagrant/Desktop/
 cp /usr/share/applications/gimp.desktop /home/vagrant/Desktop/
-
-## start LXDE
-/etc/init.d/lxdm start
